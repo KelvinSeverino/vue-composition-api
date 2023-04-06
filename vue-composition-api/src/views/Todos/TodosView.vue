624556d9eb6397f8d3e4230a332968c247ddf376
Kelvin Severino <kseveri@outlook.com>
2023-04-06 20:40:47 -0300

    Preloader na View

--- vue-composition-api/src/views/Todos/TodosView.vue
+++ vue-composition-api/src/views/Todos/TodosView.vue
@@ -1,6 +1,10 @@
 <template>
     <h1>Lista de Tarefas</h1>
 
+    <div v-show="loading">
+        Carregando as tarefas...
+    </div>
+
     <ul>
         <li v-for="todo in todos" :key="todo.id">
             {{ todo.title }}
@@ -20,16 +24,22 @@ export default {
     setup() {
         const todos = ref([])
 
+        const loading = ref(false)
+
         onMounted(() => {
+            loading.value = true
+
             TodoService.getAll()
                     .then(response => { //Quando da sucesso
                         console.log(response)
                         todos.value = response.data.data //recebe retorno da API
                     })
-                    .catch(error => console.log(error)) //Falha
+                    .catch(error => console.log(error)) //Falha na faixa de 400 ou 500
+                    .finally(() => loading.value = false) //Quando finaliza, idependente de erro ou falha
         })
 
         return {
+            loading,
             todos,
         }
     }
